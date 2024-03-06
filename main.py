c43c4a0c7c1a61b2f729e73455dc9e380071fec7
Henrique Tschannerl <h.tschannerl@gmail.com>
2024-03-05 22:23:20 -0500

    first commit

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import logging
 import warnings
 import datetime
 import os
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def getItemLibrary(libId):
+    logging.info("Getting the items of library ID " + libId)
+    api_url = "http://plex.tschannerl.com:32400/library/sections/" + libId + "/all"
+    my_headers = {'X-Plex-Token': apikey, 'Accept': 'application/json'}
+
+    response = requests.get(api_url,headers=my_headers,verify=False)
+    json_response = response.json()
+    if response.status_code == 200:
+        return json_response
+    else:
+        logging.info(json_response)
+        return "Error"
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    apikey = input("Inform the API KEY: ")
     warnings.filterwarnings('ignore')
     moment = datetime.datetime.now().strftime('%Y-%m-%m-%H%M%S')
     logdir = "./logs/"
@@ -24,6 +34,9 @@ if __name__ == '__main__':
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.DEBUG)
 
-    print_hi('PyCharm')
+    items = getItemLibrary('25')
+    for item in items['MediaContainer']['Metadata']:
+        totalMedia = len(item['Media'])
+        #for media in item['Media']:
+            #print(len(media['Part']))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
